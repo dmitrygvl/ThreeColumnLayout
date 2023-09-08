c2b7bc03fe3a0849b97277e39e3ba6b3d9b78a23
dmitrygvl <dmitrygv1994@yandex.ru>
2023-09-08 16:28:28 +0300

    Add action deploy-to-gh-pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "eslint-config-prettier"],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["import"],
   overrides: [
     {
       env: {
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
